ac1428fbec8052bb49fb75e8c1f41a57fe98ead5
NTI-AB <186269990+Chalmers-AB@users.noreply.github.com>
2026-09-15 22:30:16 +0200

    Lagade och lade till countwords i wordfreq

--- lab1/wordfreq.py
+++ lab1/wordfreq.py
@@ -10,19 +10,20 @@ def get_type(letter):
 
 def tokenize(lines):
     words = []
+    #Ba så den gör alla lines efter varandra.
     for line in lines:
         wordstart = 0
         previoustype = "s"
-
+        #Kör en i för varje bokstav så man kan köra line[i]
         for i in range(len(line)):
             currenttype = get_type(line[i])
-
+            #om förra typen inte är samma som nuvarande typ, och förra typen inte är space, så lägg till ordet i listan
             if currenttype != previoustype:
                 if previoustype != "s":
                     words.append(line[wordstart:i].lower())
 
                 wordstart = i
-
+            #just so if we just made a new word and got a space it will skip that and go to the next for wordstart
             if currenttype == "s":
                 wordstart = i + 1
 
@@ -41,3 +42,18 @@ def tokenize(lines):
         
      
 
+def countWords(arr, stopWords):
+    dic = {}
+    #kör för varje ord i listan vi inputar
+    for word in arr:
+        #om den är i stopwords ignorera den
+        if word in stopWords:
+            pass
+        #om den inte redan finns i dictionary
+        elif word not in dic:
+            #initierar hashen
+            dic[word] = 1
+        else:
+            #ökar hashets värde med 1
+            dic[word] += 1
+    return dic
